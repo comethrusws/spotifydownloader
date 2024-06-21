39d962a34e1842f1397f786b2db05f0486be50cf
comethrusws <basabjha@naver.com>
2024-06-21 21:01:29 +0545

    Commit #1.1: Search bar, Homepage (draft), contexts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
+import Search from "./components/Search";
 
 export default function Home() {
   return (
-    <div>Hello from Home</div>
+    <main className="w-full h-full flex items-center justify-center">
+      <div className="px-5 w-full md:w-[700px] space-y-5">
+        <div className="text-text">
+          <br />
+          <h1 className="text-4xl font-bold">SPOTIFY DOWNLOADER</h1>
+        </div>
+        <Search />
+      </div>
+    </main>
   );
 }
