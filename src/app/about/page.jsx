e557e2e6b0196da5f5de093c53c11bcaf0d203d6
comethrusws <basabjha@naver.com>
2024-06-23 22:47:03 +0545

    Commit #1.4: Download api fix + about page fixtures

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -18,24 +18,10 @@ const About = () => {
           <p>
             Simply press on the three dots in a Spotify track or playlist, share
             it, copy the link, then paste the link in the input dialog on the{" "}
-            <Link href="" className="underline">
+            <Link href="/" className="underline">
               home page
             </Link>
             .
-          </p>
-          <h2 className="text-2xl font-semibold mb-3">How does it work?</h2>
-          <p>
-            The website uses the Spotify API to get data from playlists and
-            tracks. When downloading them it uses <i>youtube-sr</i> to find the
-            best match to the track, then it uses <i>ytdl-core</i> to download
-            the match, and serves it to the user in the appropriate format.
-          </p>
-          <p>
-            Tracks with metadata are converted to MP3 using <i>ffmpeg.wasm</i>{" "}
-            and decorated with the data from the Spotify API (title, album,
-            cover, artists). At the moment, only individual tracks are served
-            with metadata, due to the long time hole playlists take to process,
-            and will be added to playlists in the near future.
           </p>
         </div>
       </main>
